e099e2647908b3942fc563b2cdade8859551e9ce
hideo54 <hideo54@hideo54.com>
2016-01-25 19:18:04 +0900

    Upgrade to version 2.0: user needs to turn the led manually

--- NFC/main.py
+++ NFC/main.py
@@ -5,33 +5,23 @@ import sys
 
 clf = nfc.ContactlessFrontend('usb')
 
-was_door_closed = False
-is_door_closed = False
 should_on = False
 
 def change_led_value(flag):
     if flag == True:
         subprocess.call('irsend SEND_ONCE room-led.conf on', shell=True)
-        print 'turned on'
     else:
         subprocess.call('irsend SEND_ONCE room-led.conf off', shell=True)
-        print 'turned off'
 
 def connected(tag):
-    global is_door_closed
-    is_door_closed = True
+    global should_on
+    if should_on:
+        should_on = False
+    else:
+        should_on = True
+    change_led_value(should_on)
 
 while True:
-    was_door_closed = is_door_closed
-    is_door_closed = False
-
     after1s = lambda : time.time() - started > 1
     started = time.time()
     clf.connect(rdwr={'on-connect': connected}, terminate=after1s)
-    if is_door_closed == True and was_door_closed == False:
-        if should_on == True:
-            should_on = False
-            change_led_value(should_on)
-        else:
-            should_on = True
-            change_led_value(should_on)
